73061b65e22bc171ad516cefad041db0769d83fc
Alberto Corsín Lafuente <alberto@corsin.org>
2019-01-08 01:47:30 +0100

    base for station and module requests

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 
@@ -10,11 +11,19 @@ import (
 )
 
 const (
-	APIUrl     = "https://api.netatmo.com"                              //Netatmo API Endpoint
+	APIUrl     = "https://api.netatmo.com"                              // Netatmo API Endpoint
 	UserAgent  = "nerdatmo/v0.1 (https://github.com/acorsinl/nerdatmo)" // User-Agent for requests
 	ListenPort = "6666"
 	Version    = "v0.1"
+	DataExpiry = 1800 // How long is the cache valid
 )
+
+type Nerdatmo struct {
+	StationData *NetatmoResponse
+	DataTime    int64
+}
+
+var nerdatmo Nerdatmo
 
 func main() {
 	// Load environment configuration
@@ -22,13 +31,19 @@ func main() {
 		log.Fatal("Error reading environment configuration: " + err.Error())
 	}
 
-	/* // Authenticate to Netatmo and grab station data
+	// Authenticate to Netatmo and cache station data
 	netatmoAuth := authenticateToNetatmo()
 	stationData := getStationData(netatmoAuth)
-	log.Println(stationData) */
+	log.Println(stationData)
+
+	// Preload all info for faster querying
+	nerdatmo.StationData = stationData
+	nerdatmo.DataTime = time.Now().UnixNano()
 
 	r := mux.NewRouter()
 	r.HandleFunc(StationsURL, GetStations).Methods("GET")
+	r.HandleFunc(StationsURL+"/{stationId}"+ModulesUrl, GetModules).Methods("GET")
+	r.HandleFunc(StationsURL+"/{stationId}"+ModulesUrl+"/{moduleId}", GetModule).Methods("GET")
 	http.Handle("/", r)
 
 	log.Println("Nerdatmo server " + Version + " running on port " + ListenPort)
--- server.go
+++ server.go
@@ -3,10 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 const (
 	StationsURL = "/stations"
+	ModulesUrl  = "/modules"
 )
 
 type Station struct {
@@ -21,19 +25,48 @@ type Module struct {
 }
 
 func GetStations(w http.ResponseWriter, r *http.Request) {
-	var station = Station{}
-	netatmoAuth := authenticateToNetatmo()
-	stationData := getStationData(netatmoAuth)
-	for _, stationElement := range stationData.Body.Devices {
+	currentTime := time.Now().UnixNano()
+	if nerdatmo.DataTime-currentTime > DataExpiry {
+		log.Println("Refreshing data")
+		netatmoAuth := authenticateToNetatmo()
+		nerdatmo.StationData = getStationData(netatmoAuth)
+		nerdatmo.DataTime = currentTime
+	}
+	// @todo check local time and double check against cache creation
+	var station = new(Station)
+	for _, stationElement := range nerdatmo.StationData.Body.Devices {
 		station.Id = stationElement.Id
 		station.Type = stationElement.Type
 		for _, moduleElement := range stationElement.Modules {
-			var module = Module{}
+			var module = new(Module)
 			module.Id = moduleElement.Id
 			module.Type = moduleElement.Type
-			station.Modules = append(station.Modules, module)
+			station.Modules = append(station.Modules, *module)
 		}
 	}
-
+	// @todo json response
 	log.Println(station)
+}
+
+func GetModules(w http.ResponseWriter, r *http.Request) {
+	var modules []Module
+	stationId := mux.Vars(r)["stationId"]
+	for _, stationElement := range nerdatmo.StationData.Body.Devices {
+		if stationElement.Id == stationId {
+			for _, moduleElement := range stationElement.Modules {
+				var module = new(Module)
+				module.Id = moduleElement.Id
+				module.Type = moduleElement.Type
+				modules = append(modules, *module)
+			}
+		}
+	}
+	log.Println(modules)
+}
+
+func GetModule(w http.ResponseWriter, r *http.Request) {
+	stationId := mux.Vars(r)["stationId"]
+	moduleId := mux.Vars(r)["moduleId"]
+
+	log.Println("Requesting data for station " + stationId + " and module " + moduleId)
 }
